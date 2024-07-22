6d3d45c627df3e019c8322280c512c16e36c9773
10ap17 <panticaleksa1710@gmail.com>
2024-07-22 15:32:15 +0200

    added unchecked optimization

---
+++ src/Unchecked.sol
@@ -0,0 +1,24 @@
+pragma solidity ^ 0.8.25;
+
+contract UncheckedNotOptimized{
+    function compare(uint256 loop)external pure returns(uint256 result){
+
+        for(uint256 i; i< loop; i++){
+            result += 1017;
+        }
+    }
+}
+
+contract UncheckeOptimized{
+    
+    function compare(uint256 loop)external pure returns(uint256 result){
+
+        for(uint256 i; i< loop;){
+            result += 1017;
+
+            unchecked{
+                i++;
+            }
+        }
+    }
+}
