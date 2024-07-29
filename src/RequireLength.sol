52f7a53d691987937612150ab5a3ab0d1488b30c
10ap17 <panticaleksa1710@gmail.com>
2024-07-29 17:16:52 +0200

    added require length optimization

---
+++ src/RequireLength.sol
@@ -0,0 +1,18 @@
+pragma solidity ^ 0.8.25;
+
+contract RequireLengthNotOptimized{
+    uint256 a = 1017;
+
+    function compare()external view{
+        require(a > 1017, "Not bigger");
+    }
+}
+
+contract RequireLengthOptimized{
+    uint256 a = 1017;
+
+    function compare()external view{
+        require(a > 1017, "Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger Not bigger ");
+    }
+
+}
